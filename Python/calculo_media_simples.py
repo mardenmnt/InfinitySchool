95bcf49922180dc67a0faaed0919ad471772e922
mardenmnt <100005797+mardenmnt@users.noreply.github.com>
2022-04-22 11:50:23 -0300

    Calcula a media de forma simples sem condicionais

---
+++ Python/calculo_media_simples.py
@@ -0,0 +1,11 @@
+"""
+Crie um programa que receba 3 notas e calcule a média dessas notas
+"""
+
+nota1 = float(input('Informe a 1 nota: '))
+nota2 = float(input('Informe a 2 nota: '))
+nota3 = float(input('Informe a 3 nota: '))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f'A média das notas é: {media:.2f}')
