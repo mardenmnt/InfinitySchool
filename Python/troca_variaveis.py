3db5a188cd30d535f46ff090d93a236bd554924d
mardenmnt <100005797+mardenmnt@users.noreply.github.com>
2022-04-22 11:53:22 -0300

    Troca valor das variaveis usando somente variaveis

---
+++ Python/troca_variaveis.py
@@ -0,0 +1,14 @@
+"""
+Crie duas variáveis, atribua valor a cada uma e troque os seus valores, usando somente variáveis
+"""
+
+A = 10
+B = 20
+
+# cria uma variável temporária para armazenar valor de B
+temp = B
+B = A
+A = temp
+
+print('O valor de A é:', A)
+print('O valor de B é:', B)
