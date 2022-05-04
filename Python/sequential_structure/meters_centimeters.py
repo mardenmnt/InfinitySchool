6050aa7a27a3ee53af3a19a89c18677f19227cb0
mardenmnt <marden.mnt@gmail.com>
2022-05-04 12:35:00 -0300

    meters_centimeters

---
+++ Python/sequential_structure/meters_centimeters.py
@@ -0,0 +1,9 @@
+"""
+Faça um Programa que converta metros para centímetros.
+"""
+
+metros = float(input('Informe a distância em Metros: '))
+
+centimetros = metros / 100
+
+print(f'{metros}m equivale a {centimetros}cm')
