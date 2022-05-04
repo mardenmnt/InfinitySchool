512bd230900ebda0ab258644b2c9431d74aefaaf
mardenmnt <marden.mnt@gmail.com>
2022-05-04 12:07:21 -0300

    add_numbers

---
+++ Python/sequential_structure/add_numbers.py
@@ -0,0 +1,10 @@
+"""
+Faça um Programa que peça dois números e imprima a soma
+"""
+
+num1 = int(input('Informe um número: '))
+num2 = int(input('Informe outro número: '))
+
+soma = num1 + num2
+
+print(f'A soma de {num1} + {num2} é: {soma}')
