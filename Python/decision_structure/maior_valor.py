8d2463b117166c61a7b18837ddecab97c24c3085
mardenmnt <marden.mnt@gmail.com>
2022-05-10 01:08:27 -0300

    Informa o maior valor

--- Python/decision_structure/maior_valor.py
+++ Python/decision_structure/maior_valor.py
@@ -5,6 +5,7 @@ Crie um programa que leia 2 numeros e imprima o maior
 num1 = float(input('\nInforme 1º número: ').replace(',', '.'))
 num2 = float(input('Informe 2º número: ').replace(',', '.'))
 
+# compara os valores
 if num1 > num2:
     print('O maior número é', num1)
 elif num2 > num1:
