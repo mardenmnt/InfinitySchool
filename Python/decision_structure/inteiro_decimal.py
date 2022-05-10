4ccc3e5f936b88dceb8f7a590507509e666c87d8
mardenmnt <marden.mnt@gmail.com>
2022-05-10 01:08:19 -0300

    Informa se o número é inteiro ou decimal

--- Python/decision_structure/inteiro_decimal.py
+++ Python/decision_structure/inteiro_decimal.py
@@ -4,6 +4,7 @@ Faça um Programa que peça um número e informe se o número é inteiro ou deci
 
 num = float((input('Informe numero: ')).replace(',', '.'))
 
+# verifica se é inteiro
 if num % 1 == 0:
     print('Inteiro')
 else:
