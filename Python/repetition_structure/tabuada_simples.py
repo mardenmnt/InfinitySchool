101dcb757081bd5a78e43f34c9e893e8e4796614
mardenmnt <marden.mnt@gmail.com>
2022-09-07 01:33:31 -0300

    tabuada simples

---
+++ Python/repetition_structure/tabuada_simples.py
@@ -0,0 +1,16 @@
+"""
+Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número
+inteiro entre 1 a 10. O usuário deve informar de qual numero ele deseja ver a tabuada.
+A saída deve ser conforme o exemplo abaixo:
+Tabuada de 5:
+5 X 1 = 5
+5 X 2 = 10
+...
+5 X 10 = 50
+"""
+
+numero = int(input('\nInforme numero: '))
+cont = 0
+while cont <= 9:
+    print(f'{numero} X {cont} = {numero * cont}')
+    cont += 1
